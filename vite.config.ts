bf651df0ce148d7ec42b7c2a0a9a93e4f05baa84
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2024-06-14 11:11:09 +0200

    Added GH pages stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
+import { vitePlugin as remix } from '@remix-run/dev';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
@@ -13,4 +13,7 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
+  build: {
+    outDir: 'dist',
+  },
 });
