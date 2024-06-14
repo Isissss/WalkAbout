2017bce9b9079959c2a28c777dfd44e53e4be3fb
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2024-06-14 11:54:36 +0200

    Fixes regarding GH pages stuff

---
+++ remix.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+    appDirectory: "app",
+    assetsBuildDirectory: "public/build",
+    serverBuildPath: "build/index.js",
+    publicPath: "/build/",
+    serverModuleFormat: "esm",
+};
