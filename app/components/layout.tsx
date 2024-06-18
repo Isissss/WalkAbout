46d65fc3efede289e7de782c3eba2161c5ea2cd1
Isis <1036029@hr.nl>
2024-06-18 14:17:35 +0200

    feat(menu): fix menu

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -4,7 +4,7 @@ export function LayoutUI() {
   return (
     <div className='sm:custom-grid h-full min-h-screen w-full sm:grid'>
       <Navbar />
-      <main className='container mx-auto flex flex-1 flex-col gap-4 px-4 pt-0 lg:gap-6 lg:p-6 lg:pt-0'>
+      <main className='container mx-auto flex flex-1 flex-col gap-4 p-4 pt-0 lg:gap-6 lg:p-6 lg:pt-0'>
         <Outlet />
       </main>
     </div>
