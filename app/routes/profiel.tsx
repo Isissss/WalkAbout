8b45dfd3309987d8a6b354a425a42b1b5306107c
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2024-06-18 12:32:56 +0200

    Mobile styling for profile page

--- app/routes/profiel.tsx
+++ app/routes/profiel.tsx
@@ -22,8 +22,8 @@ const Profiel = () => {
   return (
     <>
       <PageHeader title='Account' />
-      <div className='flex w-full gap-2'>
-        <div className='flex w-1/2 flex-col gap-2'>
+      <div className='flex w-full gap-2 max-lg:block'>
+        <div className='flex w-1/2 max-lg:w-full flex-col gap-2'>
           <div className='flex justify-between rounded-3xl border-2 border-foreground p-2 text-black no-underline'>
             <div className='text-2xl font-bold'>
               Jolanda
@@ -36,7 +36,7 @@ const Profiel = () => {
             <img
               src='/women.png'
               alt=''
-              className='h-52 w-72 rounded-3xl object-cover'
+              className='h-52 w-72 max-lg:w-52 rounded-3xl object-cover'
             />
           </div>
           <div className='flex justify-center rounded-3xl border-2 border-foreground p-2 text-center text-black no-underline'>
@@ -46,118 +46,122 @@ const Profiel = () => {
             </div>
           </div>
           <div className='flex justify-center rounded-3xl border-2 border-foreground p-2 text-center text-black no-underline'>
-            <div className='text-2xl font-bold'>
-              Recente wandelroutes
-              <a
-                className='block grid w-full grid-cols-4 rounded-3xl border-2 border-foreground text-black no-underline'
-                href='/routes/21'
-              >
-                <div className='col-span-1 overflow-hidden'>
-                  <img
-                    src={dummy}
-                    alt=''
-                    className='h-full w-full rounded-l-3xl object-cover'
-                  />
-                </div>
-                <div className='col-span-3 flex flex-col space-y-3 p-5'>
-                  <h2 className='text-3x col-span-3 text-3xl text-secondary'>
-                    Regio Blaak
-                  </h2>
-                  <table className='w-full text-left'>
-                    <tr>
-                      <th className='flex w-10'>Afstand:</th>
-                      <td>4 kilometer</td>
-                    </tr>
-                    <tr>
-                      <th className='flex w-10'>Moeilijkheid:</th>
-                      <td>2</td>
-                    </tr>
-                  </table>
-                </div>
-              </a>
-              <a
-                className='mt-2 block grid w-full grid-cols-4 rounded-3xl border-2 border-foreground text-black no-underline'
-                href='/routes/21'
-              >
-                <div className='col-span-1 overflow-hidden'>
-                  <img
-                    src={dummy}
-                    alt=''
-                    className='h-full w-full rounded-l-3xl object-cover'
-                  />
-                </div>
-                <div className='col-span-3 flex flex-col space-y-3 p-5'>
-                  <h2 className='text-3x col-span-3 text-3xl text-secondary'>
-                    Regio Blaak
-                  </h2>
-                  <table className='w-full text-left'>
-                    <tr>
-                      <th className='flex w-10'>Afstand:</th>
-                      <td>4 kilometer</td>
-                    </tr>
-                    <tr>
-                      <th className='flex w-10'>Moeilijkheid:</th>
-                      <td>2</td>
-                    </tr>
-                  </table>
-                </div>
-              </a>
-            </div>
+              <div className='text-2xl font-bold w-full'>
+                  Recente wandelroutes
+                  <a
+                      className='block w-full rounded-3xl border-2 border-foreground text-black no-underline'
+                      href='/routes/21'
+                  >
+                      <div className='col-span-1 overflow-hidden max-md:hidden'>
+                          <img
+                              src={dummy}
+                              alt=''
+                              className='h-full w-full md:h-52  rounded-3xl object-cover'
+                          />
+                      </div>
+                      <div className='col-span-3 flex flex-col space-y-3 p-5'>
+                          <h2 className='text-3x col-span-3 text-3xl text-secondary'>
+                              Regio Blaak
+                          </h2>
+                          <table className='w-full text-left'>
+                              <tr>
+                                  <th className='flex w-10 min-w-12'>Afstand:</th>
+                                  <td>4 kilometer</td>
+                              </tr>
+                              <tr>
+                                  <th className='flex w-10 min-w-12'>Moeilijkheid:</th>
+                                  <td>2</td>
+                              </tr>
+                          </table>
+                      </div>
+                  </a>
+                  <a
+                      className='block w-full rounded-3xl border-2 border-foreground text-black no-underline'
+                      href='/routes/21'
+                  >
+                      <div className='col-span-1 overflow-hidden max-md:hidden'>
+                          <img
+                              src={dummy}
+                              alt=''
+                              className='h-full w-full md:h-52  rounded-3xl object-cover'
+                          />
+                      </div>
+                      <div className='col-span-3 flex flex-col space-y-3 p-5'>
+                          <h2 className='text-3x col-span-3 text-3xl text-secondary'>
+                              Regio Blaak
+                          </h2>
+                          <table className='w-full text-left'>
+                              <tr>
+                                  <th className='flex w-10 min-w-12'>Afstand:</th>
+                                  <td>4 kilometer</td>
+                              </tr>
+                              <tr>
+                                  <th className='flex w-10 min-w-12'>Moeilijkheid:</th>
+                                  <td>2</td>
+                              </tr>
+                          </table>
+                      </div>
+                  </a>
+              </div>
           </div>
         </div>
-        <div className='flex w-1/2 justify-center rounded-3xl border-2 border-foreground p-2 text-center text-black no-underline'>
-          <div className='w-full text-2xl font-bold'>
-            Vouchers
-            <div className='text-xl font-normal'>
-              <div className='flex gap-3 rounded-3xl border-2 border-foreground p-2 text-black no-underline'>
-                <img
-                  src='/coffee.jpg'
-                  alt=''
-                  className='h-52 w-72 rounded-3xl object-cover'
-                />
-                <div className='text-left text-2xl font-bold'>
-                  Voucher
+          <div
+              className='flex w-1/2 max-lg:w-full justify-center rounded-3xl border-2 border-foreground p-2 text-center text-black no-underline'>
+              <div className='w-full text-2xl font-bold'>
+                  Vouchers
                   <div className='text-xl font-normal'>
-                    50% korting op een warme drank (Koffie, thee)
+                      <div className='flex gap-3 rounded-3xl border-2 border-foreground p-2 text-black no-underline'>
+                          <img
+                              src='/coffee.jpg'
+                              alt=''
+                              className='h-52 w-72 max-md:w-24 max-md:h-32 rounded-3xl object-cover'
+                          />
+                          <div className='text-left text-2xl font-bold'>
+                              Voucher
+                              <div className='text-xl font-normal'>
+                                  50% korting op een warme drank (Koffie, thee)
+                              </div>
+                          </div>
+                      </div>
                   </div>
-                </div>
-              </div>
-            </div>
-            <div className='text-xl font-normal'>
-              <div className='mt-2 flex gap-3 rounded-3xl border-2 border-foreground p-2 text-black no-underline'>
-                <img
-                  src='/coffee.jpg'
-                  alt=''
-                  className='h-52 w-72 rounded-3xl object-cover'
-                />
-                <div className='text-left text-2xl font-bold'>
-                  Voucher
                   <div className='text-xl font-normal'>
-                    50% korting op een warme drank (Koffie, thee)
+                      <div
+                          className='mt-2 flex gap-3 rounded-3xl border-2 border-foreground p-2 text-black no-underline'>
+                          <img
+                              src='/coffee.jpg'
+                              alt=''
+                              className='h-52 w-72 max-md:w-24 max-md:h-32 rounded-3xl object-cover'
+                          />
+                          <div className='text-left text-2xl font-bold'>
+                              Voucher
+                              <div className='text-xl font-normal'>
+                                  50% korting op een warme drank (Koffie, thee)
+                              </div>
+                          </div>
+                      </div>
                   </div>
-                </div>
-              </div>
-            </div>
-            <div className='text-xl font-normal'>
-              <div className='mt-2 flex gap-3 rounded-3xl border-2 border-foreground p-2 text-black no-underline'>
-                <img
-                  src='/coffee.jpg'
-                  alt=''
-                  className='h-52 w-72 rounded-3xl object-cover'
-                />
-                <div className='text-left text-2xl font-bold'>
-                  Voucher
                   <div className='text-xl font-normal'>
-                    50% korting op een warme drank (Koffie, thee)
+                      <div
+                          className='mt-2 flex gap-3 rounded-3xl border-2 border-foreground p-2 text-black no-underline'>
+                          <img
+                              src='/coffee.jpg'
+                              alt=''
+                              className='h-52 w-72 max-md:w-24 max-md:h-32 rounded-3xl object-cover'
+                          />
+                          <div className='text-left text-2xl font-bold'>
+                              Voucher
+                              <div className='text-xl font-normal'>
+                                  50% korting op een warme drank (Koffie, thee)
+                              </div>
+                          </div>
+                      </div>
                   </div>
-                </div>
               </div>
-            </div>
           </div>
-        </div>
       </div>
-      <section className='w-24 rounded-3xl border-2 border-foreground bg-orange-500 p-2 text-center text-2xl font-bold text-black no-underline'>
-        <Form action='/sign-out' method='post'>
+        <section
+            className='w-24 rounded-3xl border-2 border-foreground bg-orange-500 p-2 text-center text-2xl font-bold text-black no-underline'>
+            <Form action='/sign-out' method='post'>
           <button type='submit'>Uitloggen</button>
         </Form>
       </section>
